aff3ee186127ac9d601b690333437a473923046b
drewskis <drewskis@umich.edu>
2022-02-03 16:48:57 -0500

    initial commit

---
+++ MichWordle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MichWordle
+//
+//  Created by Drew Scheffer on 2/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
