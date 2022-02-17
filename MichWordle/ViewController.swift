4b6278078ee274fe1c8d49e7bf778ab8f4c35a0b
drewskis <drewskis@umich.edu>
2022-02-17 15:10:57 -0500

    Rearranged labels in animation

--- MichWordle/ViewController.swift
+++ MichWordle/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var board: UIStackView!
+        
+    var boardLabels : [[UILabel]] = Array(repeating: Array(repeating: UILabel(), count: 5), count: 6)
     
     let cellBoarderColor = UIColor.init(red: 0.24, green: 0.24, blue: 0.24, alpha: 1).cgColor
     
@@ -17,11 +19,27 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         init_board()
+        fillBoardLabels()
+        
+        boardLabels[2][3].text = "%"
+    }
+    
+    
+    
+    func fillBoardLabels() {
+        let rows = board.subviews.filter({$0 is UIStackView})
+        for i in 0..<rows.count{
+            let curr_row = rows[i]
+            let labels = curr_row.subviews.filter({$0 is UILabel})
+            for j in 0..<labels.count {
+                let curr_label = labels[j] as! UILabel
+                boardLabels[i][j] = curr_label
+            }
+        }
     }
     
     //Draws borders and gets rid of text
     func init_board() {
-        
         let rows = board.subviews.filter({$0 is UIStackView})
         for row in rows{
             let labels = row.subviews.filter({$0 is UILabel})
